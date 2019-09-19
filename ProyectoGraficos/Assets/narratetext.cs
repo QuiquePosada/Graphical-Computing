b171a3400d4032eb95ee84eb96565ff72d2d89a2
QuiquePosada <33169548+QuiquePosada@users.noreply.github.com>
2019-09-19 01:05:33 -0500

    UI started

---
+++ ProyectoGraficos/Assets/narratetext.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class narratetext : MonoBehaviour
+{
+
+	public InputField story;
+	public Text allstory;
+
+	public void setget(){
+		allstory.text = allstory.text + "  " + story.text;
+	}
+    // Start is called before the first frame update
+    // void Start()
+    // {
+        
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+}
